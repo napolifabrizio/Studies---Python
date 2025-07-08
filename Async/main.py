f43e00c940931c5bf1fea3cc82f5f7e28dab95ee
Fabrizio Napoli <fabrizionapoli50@gmail.com>
2025-07-08 17:58:26 -0300

    refactor: correct use of abstract classes

--- Async/main.py
+++ Async/main.py
@@ -16,15 +16,13 @@ class Async(AbstractDict):
     """
     Classe que implementa a interface AbstractDict para mapear inteiros e listas de inteiros.
     """
-    @abstractmethod
-    async def int_dict(ids_list: list) -> dict:
+    async def int_dict(self, ids_list: list) -> dict:
         mapped_ints = Counter(ids_list)  # Usando Counter para contar ocorrências
         await asyncio.sleep(5) # Simulando um processamento demorado
         print(f"Mapeamento de inteiros: {dict(mapped_ints)}")
         return dict(mapped_ints)
 
-    @abstractmethod
-    async def list_dict(*ids: int) -> dict:
+    async def list_dict(self, *ids: int) -> dict:
         mapped_ints = defaultdict(list)
         for i in ids:
             mapped_ints[i].append(i)
@@ -36,15 +34,13 @@ class Sync(AbstractDict):
     """
     Classe que implementa a interface AbstractDict para mapear inteiros e listas de inteiros.
     """
-    @abstractmethod
-    def int_dict(ids_list: list) -> dict:
+    def int_dict(self, ids_list: list) -> dict:
         mapped_ints = Counter(ids_list)  # Usando Counter para contar ocorrências
         time.sleep(5) # Simulando um processamento demorado
         print(f"Mapeamento de inteiros: {dict(mapped_ints)}")
         return dict(mapped_ints)
 
-    @abstractmethod
-    def list_dict(*ids: int) -> dict:
+    def list_dict(self, *ids: int) -> dict:
         mapped_ints = defaultdict(list)
         for i in ids:
             mapped_ints[i].append(i)
@@ -56,10 +52,11 @@ async def main(*args) -> None:
     await asyncio.gather(*args) # Executa as tarefas em paralelo
 
 def run_async():
+    async_class = Async()
     numbers = [1, 1, 1, 3, 3, 2, 2, 4, 5, 5, 5, 5, 5]
     tasks = [
-        Async.int_dict(numbers),
-        Async.list_dict(*numbers)
+        async_class.int_dict(numbers),
+        async_class.list_dict(*numbers)
     ]
     start = time.time()
     asyncio.run(main(*tasks))
@@ -67,10 +64,11 @@ def run_async():
     print(f"Tempo total da execução assíncrona: {end - start:.2f} segundos\n")
 
 def run_sync():
+    sync_class = Sync()
     numbers = [1, 1, 1, 3, 3, 2, 2, 4, 5, 5, 5, 5, 5]
     start = time.time()
-    Sync.int_dict(numbers)
-    Sync.list_dict(*numbers)
+    sync_class.int_dict(numbers)
+    sync_class.list_dict(*numbers)
     end = time.time()
     print(f"Tempo total de execução síncrona: {end - start:.2f} segundos\n")
 
